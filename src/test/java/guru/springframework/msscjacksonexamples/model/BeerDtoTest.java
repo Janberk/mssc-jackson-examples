3016f384fceabac3e8ac8c8466dc7c1fe309b8ff
Canberk Demirkan <canberk.demirkan@mtc.berlin>
2020-11-28 23:11:48 +0100

    Serialize and deserialize Dto with Jackson

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import java.io.IOException;
+
+@JsonTest
+class BeerDtoTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+
+    @Test
+    void testDeserializeDto() throws IOException {
+        String json = "{\"id\":\"ff5eb14a-7af0-4423-877e-20f3b50dca99\",\"beerName\":\"BeerName\"," +
+                "\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-11-28T23:07:26" +
+                ".8419221+01:00\",\"lastUpdatedDate\":\"2020-11-28T23:07:26.8419221+01:00\"}";
+
+        BeerDto dto = objectMapper.readValue(json, BeerDto.class);
+
+        System.out.println(dto);
+    }
+}
