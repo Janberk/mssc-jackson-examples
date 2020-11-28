06fa2f6ce944bc85642ad33486f08cdf2ed43536
Canberk Demirkan <canberk.demirkan@mtc.berlin>
2020-11-28 23:58:44 +0100

    Use JsonFormat for dates

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -20,9 +20,9 @@ class BeerDtoTest extends BaseTest {
 
     @Test
     void testDeserializeDto() throws IOException {
-        String json = "{\"beerId\":\"ff5eb14a-7af0-4423-877e-20f3b50dca99\",\"beerName\":\"BeerName\"," +
-                "\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-11-28T23:07:26" +
-                ".8419221+01:00\",\"lastUpdatedDate\":\"2020-11-28T23:07:26.8419221+01:00\"}";
+        String json = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":\"12.99\"," +
+                "\"createdDate\":\"2020-11-28T23:55:44+0100\",\"lastUpdatedDate\":\"2020-11-28T23:55:44" +
+                ".4441706+01:00\",\"beerId\":\"9a901b09-b146-4899-81d7-e2f615cd886d\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
